9e410e03b9c4a114ca1375f16ed4e8e6129f6b46
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2015-09-14 14:16:39 +0200

    Added Slack webhook

--- routes/the_rules.rb
+++ routes/the_rules.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'net/https'
+
 class CiberSykkel < Sinatra::Application
   unless rules_token = ENV['VELO_RULES_TOKEN']
     raise "You must specify the VELO_RULES_TOKEN env variable"
@@ -15,6 +18,22 @@ class CiberSykkel < Sinatra::Application
     
     logger.info "Posting rule ##{rule_id}"
 
-    the_rules[rule_id]
+    rule = the_rules[rule_id]
+
+    https = Net::HTTP.new(SLACK_URL.host, SLACK_URL.port)
+    https.use_ssl = true
+    request = Net::HTTP::Post.new(SLACK_URL.path)
+    request.body = {
+      "channel": "#squash",
+      "username": "SquashBot",
+      "attachments": [
+                      {
+                        "fallback": rule["rule"],
+                        "color": "good",
+                        "title": "Rule ##{rule_id}",
+                        "text": "#{rule["rule"]}\n#{rule["description"]}"
+                      }
+                     ]}.to_json
+    logger.info https.request(request)
   end
 end
